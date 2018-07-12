5235a4019514e6cd575294778fe8f1ad6f033e85
Yevhenii <32123288+silver12666@users.noreply.github.com>
2018-07-12 08:37:30 +0300

    Update DataLine.java

--- src/DataLine.java
+++ src/DataLine.java
@@ -5,7 +5,7 @@ public class DataLine extends Line {
     private Date date;
     private int waitingTime;
 
-    DataLine(java.lang.String serviceVariation, java.lang.String typeCategorySubcategory, String responseType, Date date, int waitingTime) {
+    DataLine(String serviceVariation, String typeCategorySubcategory, String responseType, Date date, int waitingTime) {
         super(serviceVariation, typeCategorySubcategory, responseType);
         this.date = date;
         this.waitingTime = waitingTime;
